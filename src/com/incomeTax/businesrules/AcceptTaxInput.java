8a85900edfeca5fc7f45cc22d82f6abe7720f613
swathi <swathi@192.168.31.60>
2018-06-25 21:40:01 +0530

    New project

---
+++ src/com/incomeTax/businesrules/AcceptTaxInput.java
@@ -0,0 +1,27 @@
+package com.incomeTax.businesrules;
+
+public class AcceptTaxInput {
+	
+	Long salary = null;
+	Long houseRentAllowance = null;
+	Long transportAllowance = null;
+	Long medicalAllowance = null;
+	Long bonus = null;
+	Long leaveTravelAllowance = null;
+	Long dearnessAllowance = null;
+	Long performanceBonus = null;
+	
+
+	public static void main(String[] args) {
+		
+		AcceptTaxInput acceptTaxInput = new AcceptTaxInput();
+		
+		
+	}
+	
+	public static void calculateTax() {
+		
+	}
+	
+	
+}
